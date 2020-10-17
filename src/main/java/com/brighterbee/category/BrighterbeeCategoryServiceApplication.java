598607f1b9688bb127b3ee58efb90213435e980c
Ashwin Patil <ashwin.patil@mindtree.com>
2020-10-17 17:25:36 +0530

    added commands

--- src/main/java/com/brighterbee/category/BrighterbeeCategoryServiceApplication.java
+++ src/main/java/com/brighterbee/category/BrighterbeeCategoryServiceApplication.java
@@ -1,12 +1,19 @@
 package com.brighterbee.category;
 
+import java.util.stream.Stream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class BrighterbeeCategoryServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println(args);
+		Stream.of(args).forEach(a -> log.info(a));
 		SpringApplication.run(BrighterbeeCategoryServiceApplication.class, args);
 	}
 }
