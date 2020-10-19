32a23acd3d1071ccfabd671aa1e7a58c6043515c
Ashwin Patil <ashwin.patil@mindtree.com>
2020-10-19 11:38:47 +0530

    update kuberentes config dependency

--- src/main/java/com/brighterbee/category/controller/GreetingController.java
+++ src/main/java/com/brighterbee/category/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package com.brighterbee.category.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
+	
+	@Value("${app}")
+	private String app;
 
 	@GetMapping
 	public String greet() {
-		return "Hello, Good Evening";
+		return "Hello, Good Evening " + app;
 	}
 
 }
