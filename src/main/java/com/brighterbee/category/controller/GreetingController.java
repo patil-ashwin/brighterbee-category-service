7e3584ae1c2c1e615c1134b64be9725d5459d8bd
Ashwin Patil <ashwin.patil@mindtree.com>
2020-10-21 15:06:52 +0530

    updated vol

--- src/main/java/com/brighterbee/category/controller/GreetingController.java
+++ src/main/java/com/brighterbee/category/controller/GreetingController.java
@@ -1,22 +1,41 @@
 package com.brighterbee.category.controller;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/greeting")
 @RefreshScope
+@Slf4j
 public class GreetingController {
-	
+
 	@Value("${app}")
 	private String app;
 
 	@GetMapping
 	public String greet() {
-		return "Hello, Good Evening " + app;
+		File file = new File(app + "secret.txt");
+		try {
+			boolean isFileCreated = file.createNewFile();
+			if (isFileCreated) {
+				try (FileWriter writter = new FileWriter(file)) {
+					writter.write("Hello, It's secret!");
+					writter.flush();
+				}
+			}
+		} catch (IOException e) {
+			log.error(e.getLocalizedMessage());
+		}
+		return "Hello, Good Evening ";
 	}
 
 }
